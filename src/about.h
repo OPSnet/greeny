3e1a87fe1a3c4da2003d681112b23977b1ba858f
Welp Wazowski <welp@apollo.rip>
2018-11-05 17:35:19 -0800

    bump version

--- src/about.h
+++ src/about.h
@@ -1,7 +1,7 @@
 #ifndef H_ABOUT
 #define H_ABOUT
 
-#define GRN_VERSION "orpheus-rc1"
+#define GRN_VERSION "orpheus-1.0.0"
 #define GRN_ABOUT_TEXT "Greeny version " GRN_VERSION "\n" \
 	"Greeny is a tool for modifying .torrent files in bulk.\n" \
 	"Copyleft (?) 2018 the Orpheus developers.\n" \
